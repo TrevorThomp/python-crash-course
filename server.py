da30ba6723ff9e1d78a795c8fbf2327bbc760f24
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-30 10:17:49 -0800

    added text for home and goodbye routes

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 from http.server import BaseHTTPRequestHandler
+from routes.main import routes
 class Server(BaseHTTPRequestHandler):
+
   def do_HEAD(self):
     return
+
   def do_POST(self):
     return
+
   def do_GET(self):
     self.respond()
+
   def handle_http(self, status, content_type):
     self.send_response(status)
     self.send_header('Content-type', content_type)
     self.end_headers()
-    return bytes('Hello World', 'UTF-8')
+    route_content = routes[self.path]
+    return bytes(route_content, 'UTF-8')
     
   def respond(self):
     content = self.handle_http(200, 'text/html')
