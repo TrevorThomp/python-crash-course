c76b5c3fbee6d575c5ae8364ce5818ae5651d9a3
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-30 09:50:45 -0800

    fixed httpd variable

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
   httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
   print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
   try:
-    httpd:serve_forever()
+      httpd.serve_forever()
   except KeyboardInterrupt:
     pass 
   httpd.server_close()
--- server.py
+++ server.py
@@ -8,10 +8,10 @@ class Server(BaseHTTPRequestHandler):
     return
   def handle_http(self, status, content_type):
     self.send_response(status)
-    self.send_header(‘Content-type’, content_type)
+    self.send_header('Content-type', content_type)
     self.end_headers()
-    return bytes(“Hello World”, “UTF-8”)
+    return bytes('Hello World', 'UTF-8')
     
   def respond(self):
-    content = self.handle_http(200, ‘text/html’)
+    content = self.handle_http(200, 'text/html')
     self.wfile.write(content)
